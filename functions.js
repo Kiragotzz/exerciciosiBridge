b199c08a33e7cab13e085cfb5e5db5406803c1d9
Luis <luis_gabriell1@hotmail.com>
2020-07-23 13:09:05 -0300

    commit basico

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 //Functions
+//exercício2
 function ordCresc(){
     
     var numbers = [1,3,4,9,5,7,2,6,20,11,12,17];
@@ -8,7 +9,7 @@ function ordCresc(){
     });
     alert(`A resposta é: ${numbers}`);
 }
-
+//exercício3
 function ordDecresc(){
     
     var numbers = [1,3,4,9,5,7,2,6,15,12];
@@ -19,9 +20,10 @@ function ordDecresc(){
     alert(`A resposta é: ${numbers}`);
 }
 
+//exercício4
+
 function contarCarac() {
-    var stringTeste = "teste "
     var stringCarac = "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
-	document.getElementById("campoResposta").innerHTML = stringTeste.length;
+	document.getElementById("campoResposta").innerHTML = stringCarac.length;
 } 
