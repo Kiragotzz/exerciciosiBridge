d65680bb044d8610f1984248530eb8eb52481feb
Luis <luis_gabriell1@hotmail.com>
2020-07-29 11:13:09 -0300

    Finalizado fase 4

--- jquery.js
+++ jquery.js
@@ -2,11 +2,34 @@
 //listadecadastro
 $(function(){
 
+	//FILTRO
+	$(function(){
+		$("#tblListar input").keyup(function(){       
+			var index = $(this).parent().index();
+			var nth = "#tblListar td:nth-child("+(index+1).toString()+")";
+			var valor = $(this).val().toUpperCase();
+			$("#tblListar tbody tr").show();
+			$(nth).each(function(){
+				if($(this).text().toUpperCase().indexOf(valor) < 0){
+					$(this).parent().hide();
+				}
+			});
+		});
+	 
+		$("#tblListar input").blur(function(){
+			$(this).val("");
+		});
+	});
+	
+	//Mascaras
+	$("#txtCpf").mask("999.999.999-99", {reverse: false});
+	$("#txtTelefone, #txtCelular").mask("(99)99999-9999", {reverse: false});
+
 	var operacao = "A"; //"A"=Adição; "E"=Edição
 
 	var indice_selecionado = -1;
 
-	var tbClientes = localStorage.getItem("tbClientes");// Recupera os dados armazenados
+	var tbClientes = localStorage.getItem("tbClientes");// Recupera os dados
 
 	tbClientes = JSON.parse(tbClientes); // Converte string para objeto
 
@@ -14,7 +37,7 @@ $(function(){
 		tbClientes = [];
 
 	function Adicionar(){
-		var cli = GetCliente("Codigo", $("#txtCodigo").val());
+		var cli = GetCliente("Cpf", $("#txtCpf").val());
 
 		if(cli != null){
 			alert("Código já cadastrado.");
@@ -22,10 +45,14 @@ $(function(){
 		}
 
 		var cliente = JSON.stringify({
-			Codigo   : $("#txtCodigo").val(),
+			Cpf   : $("#txtCpf").val(),
 			Nome     : $("#txtNome").val(),
 			Telefone : $("#txtTelefone").val(),
-			Email    : $("#txtEmail").val()
+			Celular : $("#txtCelular").val(),
+			Email    : $("#txtEmail").val(),
+			Sexo    : $("#txtSexo").val(),
+			Nascimento    : $("#txtNasc").val(),
+			Habilidade    : $("#txtHab").val()
 		});
 
 		tbClientes.push(cliente);
@@ -38,10 +65,14 @@ $(function(){
 
 	function Editar(){
 		tbClientes[indice_selecionado] = JSON.stringify({
-				Codigo   : $("#txtCodigo").val(),
+				Cpf   : $("#txtCpf").val(),
 				Nome     : $("#txtNome").val(),
 				Telefone : $("#txtTelefone").val(),
-				Email    : $("#txtEmail").val()
+				Celular : $("#txtCelular").val(),
+				Email    : $("#txtEmail").val(),
+				Sexo    : $("#txtEmail").val(),
+				Nascimento    : $("#txtNasc").val(),
+				Habilidade    : $("#txtHab").val()
 			});
 		localStorage.setItem("tbClientes", JSON.stringify(tbClientes));
 		alert("Informações editadas.")
@@ -54,10 +85,14 @@ $(function(){
 		$("#tblListar").html(
 			"<thead>"+
 			"	<tr>"+
-			"	<th scope='col'>Código</th>"+
+			"	<th scope='col'>Cpf<input placeholder='Filtre pelo CPF' type='text' aria-label='10px' id='txtColuna1 inputGroup-sizing-sm'/></th>"+
 			"	<th scope='col'>Nome</th>"+
 			"	<th scope='col'>Telefone</th>"+
+			"	<th scope='col'>Celular</th>"+
 			"	<th>Email</th>"+
+			"	<th>Sexo</th>"+
+			"	<th>Nascimento</th>"+
+			"	<th>Habilidade</th>"+
 			"   <th></th>"+
 			"	</tr>"+
 			"</thead>"+
@@ -68,11 +103,15 @@ $(function(){
 		 for(var i in tbClientes){
 			var cli = JSON.parse(tbClientes[i]);
 		  	$("#tblListar tbody").append("<tr>"+
-              "	<td>"+cli.Codigo+"</td>" + 
+              "	<td>"+cli.Cpf+"</td>" + 
               "	<td>"+cli.Nome+"</td>" + 
-              "	<td>"+cli.Telefone+"</td>" + 
+			  "	<td>"+cli.Telefone+"</td>" + 
+			  "	<td>"+cli.Celular+"</td>" + 
               "	<td>"+cli.Email+"</td>" + 
-              "	<td><img src='edit.png' alt='"+i+"' class='btnEditar'/><img src='delete.png' alt='"+i+"' class='btnExcluir'/></td>" + 
+			  "	<td>"+cli.Sexo+"</td>" + 
+			  "	<td>"+cli.Nascimento+"</td>" + 
+			  "	<td>"+cli.Habilidade+"</td>" + 
+              "	<td><img src='img/edit.png' title='Editar' alt='"+i+"' class='btnEditar'/><img src='img/delete.png' title='Apagar' alt='"+i+"' class='btnExcluir'/></td>" + 
 		  								 "</tr>");
 		 }
 	}
@@ -106,11 +145,15 @@ $(function(){
 		operacao = "E";
 		indice_selecionado = parseInt($(this).attr("alt"));
 		var cli = JSON.parse(tbClientes[indice_selecionado]);
-		$("#txtCodigo").val(cli.Codigo);
+		$("#txtCpf").val(cli.Cpf);
 		$("#txtNome").val(cli.Nome);
 		$("#txtTelefone").val(cli.Telefone);
+		$("#txtCelular").val(cli.Celular);
 		$("#txtEmail").val(cli.Email);
-		$("#txtCodigo").attr("readonly","readonly");
+		$("#txtSexo").val(cli.Sexo);
+		$("#txtNasc").val(cli.Nascimento);
+		$("#txtHab").val(cli.Habilidade);
+		$("#txtCpf").attr("readonly","readonly");
 		$("#txtNome").focus();
 	});
 
